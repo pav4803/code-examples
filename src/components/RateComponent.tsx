bf0fc2c58ef636ef975ce5ef7945ec050c98863e
unknown <panchuk1975vadik@gmail.com>
2022-02-21 11:44:59 +0200

    Correct

--- src/components/RateComponent.tsx
+++ src/components/RateComponent.tsx
@@ -1,4 +1,4 @@
-import React, { useState, memo } from "react";
+import React, { memo } from "react";
 import { IData } from "../store/reducers/commonSlice";
 import { ReturnImput } from "../helpers/renderFunc";
 
